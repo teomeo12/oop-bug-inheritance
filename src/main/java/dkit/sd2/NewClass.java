b0f3c327bf4d0557f8713e2e9c5e2059849c4eba
teomeo12 <d00240834@student.dkit.ie>
2021-11-10 18:35:38 +0000

    add new class

---
+++ src/main/java/dkit/sd2/NewClass.java
@@ -0,0 +1,8 @@
+package dkit.sd2;
+
+public class NewClass extends HoppingBug{
+
+    public NewClass(int initialPosition, int hoppingDistance) {
+        super(initialPosition, hoppingDistance);
+    }
+}
